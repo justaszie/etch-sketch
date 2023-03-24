45b4eef5b2640d13860bffa446ef7fc9507fd000
Justas Z <justaxsz@gmail.com>
2023-03-24 10:36:55 +0400

    Changed bg color logic to use random rgb values

--- javascript.js
+++ javascript.js
@@ -2,17 +2,13 @@ const container = document.querySelector(".container");
 // Initialize the grid
 createGrid(container);
 
-// Add event listener to each box - when hovered on, add hover class to change background
-const boxList = document.querySelectorAll(".box");
-
-// Add event listener to each box - when hovered on, add hover class to change background0
+// Add event listener - when user clicks on new grid button, they enter a number and we reinitialize the grid with that number
 const newGridButton = document.querySelector("button");
 newGridButton.addEventListener('click', () => {
     let newNumber = prompt("Please enter new number of boxes");
     container.innerHTML = "";
     createGrid(container, parseInt(newNumber));
 })
-
 
 function createGrid(container, numberOfBoxes = 16) {
     for (i = 0; i < numberOfBoxes; i++) {
@@ -25,8 +21,14 @@ function createGrid(container, numberOfBoxes = 16) {
             let box = document.createElement("div");
             box.classList.add('box');
             row.appendChild(box);
+            // Add event listener to each box - when hovered on change the background with random rgb values
             box.addEventListener('mouseover',() => {
-                box.classList.add('hover');
+                //Instead of class, we will dynamically calculate the background now
+                // box.classList.add('hover');
+                let redValue = Math.floor(Math.random() * 256);
+                let greenValue = Math.floor(Math.random() * 256);
+                let blueValue = Math.floor(Math.random() * 256);
+                box.style.backgroundColor = `rgb(${redValue}, ${greenValue}, ${blueValue})`;
             });
         }
     }
